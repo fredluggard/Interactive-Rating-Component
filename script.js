39f928695edd13934c2b8a51167fa268e298fa4d
Fred <fredrick.ebuka33@gmail.com>
2023-11-16 18:05:21 +0100

    Complete Project

---
+++ script.js
@@ -0,0 +1,31 @@
+let submit = document.querySelector(".submitted");
+let form = document.querySelector(".rating-form");
+let thankYou = document.querySelector(".hidden");
+let rated = document.querySelector(".rated");
+let roundButtons = document.getElementsByClassName("round_num");
+
+let addNumChangeColor = (elementList) => {
+  for (let element of roundButtons) {
+    element.classList.remove("num");
+    element.classList.add("count_hover");
+    element.classList.remove("count_clicked");
+  }
+};
+
+//let addColor = (classList)
+for (let element of roundButtons) {
+  element.addEventListener("click", () => {
+    addNumChangeColor(roundButtons);
+    element.classList.add("num");
+    element.classList.remove("count_hover");
+    element.classList.add("count_clicked");
+    rated.innerHTML = element.innerHTML;
+  });
+}
+
+function submitHandler(e) {
+  e.preventDefault();
+  form.style.display = "none";
+  thankYou.style.display = "flex";
+}
+submit.addEventListener("click", submitHandler);
